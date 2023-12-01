b94c4db57f3824c9bd28d97d4af3e0c9440b5cb1
Lukabergs <lukabergs@gmail.com>
2023-12-01 19:07:55 +0100

    Squash bugs and add comments

--- src/main/java/eus/birt/dam/Ejercicio3.java
+++ src/main/java/eus/birt/dam/Ejercicio3.java
@@ -8,15 +8,17 @@ import java.util.regex.Pattern;
 
 public class Ejercicio3 {
     public static void main(String[] args) throws Exception {
+        // Se pide al usuario el DNI del asistente a registrar en evento
         System.out.println("Introduce el DNI del asistente:");
         Scanner sc = new Scanner(System.in);
         String dni = sc.nextLine();
         Pattern patronDni = Pattern.compile("\\d{8}\\w");
         Matcher comparaFormato = patronDni.matcher(dni);
-        if (!comparaFormato.matches()) {
+        if (!comparaFormato.matches()) { // Si el DNI no cumple con el formato adecuado, se avisa a usuario y se detiene la ejecución del programa
             System.out.println("El DNI proporcionado tiene un formato inválido.");
         } else {
-            try {
+            try { // Si el formato del DNI es válido, nos conectamos a la BD y creamos un PreparedStatement
+                
                 Connection con = DriverManager.getConnection(
                     "jdbc:mysql://localhost/dbeventos",
                     "root",
@@ -26,12 +28,12 @@ public class Ejercicio3 {
                     "SELECT nombre FROM asistentes WHERE dni = ?"
                 );
                 
-    
+                // Ejecutamos la sentencia
                 pstmt.setString(1, dni);
                 ResultSet res = pstmt.executeQuery();
                 String nombre;
     
-                if (!res.next()) {
+                if (!res.next()) { // Si no existen personas registradas con ese DNI en la BD, se pide un numbre y se registra una nueva persona con esos datos
                     System.out.println(
                         "No se encontró ningún asistente con el DNI proporcionado.\n"
                         + "Introduce el nombre del asistente:"
@@ -44,6 +46,8 @@ public class Ejercicio3 {
                 } else {
                     nombre = res.getString(1);
                 }
+
+                // Se muestra al usuario el nombre de la persona a registrar en un evento, y se proporciona la lista de eventos con sus respectivos aforos disponibles
                 System.out.printf("Estás realizando la reserva para: %s%n", nombre);
                 System.out.println("Lista de eventos:");
                 Statement stmt = con.createStatement();
@@ -62,6 +66,8 @@ public class Ejercicio3 {
                         plazasLibres.get(i++)
                     );
                 }
+
+                // Se pide al usuario el evento para un nuevo registro
                 System.out.println("Elige el número del evento al que quiere asistir:");
                 int idEvento = sc.nextInt();
     
@@ -70,17 +76,17 @@ public class Ejercicio3 {
                 pstmt.setInt(2, idEvento);
                 res = pstmt.executeQuery();
     
-                if (!res.next()) {
+                if (!res.next()) { // Si el evento tiene plazas disponibles y la persona no estaba registrada previamente, se procede con el registro
                     if (plazasLibres.get(idEvento - 1) > 0) {
                         pstmt = con.prepareStatement("INSERT INTO asistentes_eventos VALUES (?, ?)");
                         pstmt.setString(1, dni);
                         pstmt.setInt(2, idEvento);
                         pstmt.executeUpdate();
                         System.out.printf("%s ha sido registrado/a para el evento seleccionado.%n", nombre);
-                    } else {
+                    } else { // Si el aforo está completo, se avisa al usuario y se finaliza la ejecución del programa
                         System.out.println("El evento seleccionado no tiene plazas disponibles.");
                     }
-                } else {
+                } else { // Si la persona ya estaba registrada en ese evento previamente, se avisa al usuario y se finaliza la ejecución del programa
                     System.out.printf("%s ya estaba registrado/a para el evento seleccionado. Registro cancelado.%n", nombre);
                 }
     
--- src/main/java/eus/birt/dam/Ejercicio4.java
+++ src/main/java/eus/birt/dam/Ejercicio4.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Ejercicio4 {
     public static void main(String[] args) throws Exception {
         try {
+            // Nos conectamos a la BD, creamos un Statement, lo ejecutamos y mostramos la lista de eventos
             Connection con = DriverManager.getConnection(
                 "jdbc:mysql://localhost/dbeventos",
                 "root",
@@ -14,22 +15,25 @@ public class Ejercicio4 {
             
             System.out.println("Lista de eventos:");
             Statement stmt = con.createStatement();
-            String sql = "SELECT nombre_evento FROM eventos";
+            String sql = "SELECT id_evento, nombre_evento FROM eventos ORDER BY id_evento";
             ResultSet res = stmt.executeQuery(sql);
-            int i = 1;
             while (res.next()) {
-                System.out.printf("%d. %s%n", i++, res.getString(1));
+                System.out.printf("%d. %s%n", res.getInt(1), res.getString(2));
             }
             System.out.println();
 
+            // Se pide al usuario el número del evento cuya cantidad de asistentes queremos consultar
             System.out.println("Introduce el ID del evento para consultar la cantidad de asistentes:");
             Scanner sc = new Scanner(System.in);
             int id = sc.nextInt();
 
+            // Ejecutamos la función obtener_numero_asistentes almacenada en la BD
             CallableStatement cstmt = con.prepareCall("{ ? = call obtener_numero_asistentes(?) }");
             cstmt.registerOutParameter(1, Types.INTEGER);
             cstmt.setInt(2, id);
             cstmt.execute();
+
+            // Imprimimos en pantalla el resultado
             System.out.printf(
                 "El número de asistentes para el evento seleccionado es: %d%n",
                 cstmt.getInt(1)
